285d4d78f8562ea7490f6a2d8c73ffafefef4f32
Opher-Lubzens <Opher.lubzens@gmail.com>
2025-08-07 16:42:37 +0300

    add json schema testing and additional endpoints

--- test_marketstack_end_of_day.py
+++ test_marketstack_end_of_day.py
@@ -1,9 +1,11 @@
 import pytest, requests
 from jsonschema import validate
 
+import marketplace_schema
 
 API_KEY = '6a1927ea264d11abdbf882e54f5e717e'
 SYMBOL = 'AAPL'
+
 class TestMarketstackEndOfDay():
     @pytest.mark.parametrize('symbol', [(SYMBOL)])
     def test_eod_response_code_only_required(self, symbol):
@@ -23,5 +25,27 @@ class TestMarketstackEndOfDay():
     @pytest.mark.parametrize('symbol', [(SYMBOL)])
     def test_eod_schema(self, symbol):
         response = requests.get(f'https://api.marketstack.com/v2/eod/latest?'
+                                f'access_key={API_KEY}&symbols={symbol}')
+        response.raise_for_status()
+        validate(instance=response.json(), schema=marketplace_schema.EOD_SCHEMA)
+
+class TestIntraday():
+    @pytest.mark.parametrize('symbol', [(SYMBOL)])
+    def test_intraday_response_code_only_required(self, symbol):
+        response = requests.get(f'http://api.marketstack.com/v2/intraday/latest?'
+                                f'access_key={API_KEY}&symbols={symbol}')
+        assert  response.status_code == 403
+
+class TestCommodities():
+    @pytest.mark.parametrize('commodity', [('gold')])
+    def test_commodity_response_code(self, commodity):
+        response = requests.get(f'http://api.marketstack.com/v2/commodities?'
+                                f'access_key={API_KEY}&commodity_name={commodity}')
+        assert  response.status_code == 403
+
+class TestSplits():
+    @pytest.mark.parametrize('symbol', [(SYMBOL)])
+    def test_split_response_code_only_required(self, symbol):
+        response = requests.get(f'http://api.marketstack.com/v2/splits?'
                                 f'access_key={API_KEY}&symbols={symbol}')
         response.raise_for_status()
