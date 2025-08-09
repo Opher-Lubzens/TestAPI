9e66c15b363a12af50bf2d88450b2aac5508813d
Opher-Lubzens <Opher.lubzens@gmail.com>
2025-08-09 20:04:42 +0300

    fixed locust file

--- nasa_locust.py
+++ nasa_locust.py
@@ -10,8 +10,7 @@ class NASAAPODUser(HttpUser):
     wait_time = between(60, 120)
 
     @task
-    @pytest.mark.parametrize('date', [(datetime.date.today())])
-    def nasa_get_todays_apod(self,date):
-        self.client.get(f"/planetary/apod?api_key={API_KEY_NASA}&date={date}")
+    def nasa_get_todays_apod(self):
+        self.client.get(f"/planetary/apod?api_key={API_KEY_NASA}&date={datetime.date.today()}")
 
 
